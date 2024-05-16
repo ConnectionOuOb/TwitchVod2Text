65996db57391b6bb4bb00914580e508541c3a013
Connection <you@example.com>
2024-05-16 22:57:22 +0800

    Version 1.0

--- TwitchVodSub.sh
+++ TwitchVodSub.sh
@@ -8,8 +8,9 @@ else
 	while true
 	do
 		nowDate=$(date '+%Y-%m-%d_%H-%M-%S')
-		vodFileTs="$1/$(basename $2)_$nowDate.ts"
-		vodFileMp4="$1/$(basename $2)_$nowDate.mp4"
+		vodName=$(basename $2)
+		vodFileTs="$1/$vodName.$nowDate.ts"
+		vodFileMp4="$1/$vodName.$nowDate.mp4"
 
 		echo "##### Check Time : $nowDate"
 
@@ -17,7 +18,7 @@ else
 
 		if [ -f $vodFileTs ]
 		then
-			nohup ffmpeg -i "$vodFileTs" -c:v copy -c:a copy -bsf:a aac_adtstoasc -y "$vodFileMp4" ; whisper --language Chinese --model large "$vodFileMp4" --initial_prompt "以下是普通話的句子。" --output_dir $1 --output_format srt > /dev/null 2>&1 &
+			nohup ffmpeg -i "$vodFileTs" -c:v copy -c:a copy -bsf:a aac_adtstoasc -y "$vodFileMp4" ; rm "$vodFileTs" ; whisper --language Chinese --model large "$vodFileMp4" --initial_prompt "我要使用繁體中文，所以以下是普通話的句子。" --output_dir $1 --output_format srt > /dev/null 2>&1 &
 		fi
 		sleep 2
 	done
